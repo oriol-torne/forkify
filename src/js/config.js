c8bb954cec8b9532be2bdb1c60a7b67a9cc4b866
uriolot <uriolot@gmail.com>
2025-03-18 17:26:01 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// capital letters for variables that won't change
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f01a5c1a-a8f6-4a5f-a771-ae240aa685a6';
+export const MODAL_CLOSE_SEC = 2.5;
